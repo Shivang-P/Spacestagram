43f3a78974a2a61be8dcc612c136d7c1bdc9810a
Shivang-P <shivangg.patel@outlook.com>
2022-01-09 21:52:01 -0500

    added like functionality

--- spacestagram/src/components/Card.js
+++ spacestagram/src/components/Card.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart as fasHeart} from '@fortawesome/free-solid-svg-icons'
+import { faHeart as farHeart} from '@fortawesome/free-regular-svg-icons'
+
 
 function Card({ image }) {
+    const [liked, setLiked] = useState(false)
+
+    function toggleLiked() {
+        setLiked(!liked)
+    }
+
     return (
         <div class="card card-compact lg:card-normal card-bordered shadow-lg">
             <figure>
@@ -8,11 +18,13 @@ function Card({ image }) {
             </figure> 
             <div class="card-body">
                 <h2 class="card-title">{image.title} - {image.date}
-                    <div class="badge mx-2">Image</div>
                 </h2> 
+                <div class="badge mb-2">Image</div>
                 <p>{image.explanation}</p> 
                 <div class="card-actions">
-                    <button class="btn btn-primary">LIKE</button> 
+                    <button class={liked ? "btn btn-secondary" : "btn btn-secondary btn-outline"} onClick={toggleLiked}>{
+                        liked ? <FontAwesomeIcon icon={fasHeart}/> : <FontAwesomeIcon icon={farHeart}/>
+                    }</button> 
                 </div>
             </div>
         </div>
